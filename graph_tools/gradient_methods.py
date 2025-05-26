053fa95fbd8171a43fc9400149f08561a973d1eb
Juan Cervino <juancervino@dhcp-10-29-226-191.dyn.mit.edu>
2025-05-26 11:17:41 -0400

    moved         self.Sobolev = compute_sobolev_matrix(Laplacian, epsilon, beta) as a separate function by itself

--- graph_tools/gradient_methods.py
+++ graph_tools/gradient_methods.py
@@ -5,49 +5,14 @@ import torch.nn.functional as F
 from graph_tools.graph_utils import create_Dh_numpy, create_Dh_torch
 import scipy.linalg as scipy
 
-# class Sobolev:
-    
-#     def __init__(self, coefficients, Laplacian, epsilon, beta, M):
-#         # super().__init__(learning_rate)
-#         self.coefficients = coefficients
-#         self.Laplacian = Laplacian
-#         self.epsilon = epsilon
-#         self.beta = beta
-#         self.Sobolev = self.sobolev_matrix()
-#         self.Dh = create_Dh_torch(M)
-    
-#     def sobolev_matrix(self):
-#         # From MATLAB
-#         # https://github.com/jhonygiraldo/GraphTRSS/blob/main/sea_surface_temperature_experiment/evolution_loss/evolution_loss_normalized.m
-#         # sobolev_matrix = G.L+sob.epsilon_set(sob.best_epsilon)*eye(G.N);
-#         # %% Symmetrization
-#         # sobolev_matrix = 0.5*(sobolev_matrix+sobolev_matrix');
-#         # sobolev_matrix = sparse(sobolev_matrix);
-#         # TODO: Missing the power of beta
-#         sobolev = self.Laplacian + self.epsilon * np.eye(self.Laplacian.shape[0])
-#         # Missing power of beta
-#         sobolev = 0.5 * (sobolev + sobolev.T)
-#         sobolev = np.linalg.matrix_power(sobolev, self.beta)
-#         # return sobolev
-#         return torch.tensor(sobolev, dtype=torch.float32)  # Adjust dtype if needed
-#     def loss(self, x, y, mask):
-#         loss_1 = F.mse_loss(torch.mul(x,mask), y)
-#         x_diff = x @ self.Dh
-#         loss_2 = torch.trace( x_diff.T @  self.Sobolev @ x_diff)
-#         return self.coefficients[0]*loss_1 + self.coefficients[1]*loss_2
-    
-# class Tikhonov:
-#     def __init__(self, coefficients, Laplacian):
-#         self.coefficients = coefficients
-#         self.Laplacian = Laplacian
 
-#     def loss(self, x, y, mask):
-#         # x_tilde = torch.mul(x, mask)
-#         loss_mse = F.mse_loss(x[mask], y[mask])
-#         # loss_mse = F.mse_loss(torch.mul(x,mask),  y)
-#         loss_lap = torch.trace(x.T@self.Laplacian@x)
-#         return self.coefficients[0] * loss_mse + self.coefficients [1] * loss_lap
-    
+
+def compute_sobolev_matrix(Laplacian, epsilon, beta):
+    sobolev = Laplacian + epsilon * np.eye(Laplacian.shape[0])
+    sobolev = 0.5 * (sobolev + sobolev.T)
+    sobolev = scipy.fractional_matrix_power(sobolev, beta)
+    return torch.tensor(sobolev, dtype=torch.float32)
+
 class BaseLoss:
     def __init__(self, coefficient):
         self.coefficient = coefficient
@@ -67,14 +32,14 @@ class LaplacianLoss(BaseLoss):
 class SobolevLoss(BaseLoss):
     def __init__(self, coefficient, Laplacian, epsilon, beta, M):
         super().__init__(coefficient)
-        self.Sobolev = self.compute_sobolev_matrix(Laplacian, epsilon, beta)
+        self.Sobolev = compute_sobolev_matrix(Laplacian, epsilon, beta)
         self.Dh = create_Dh_torch(M)
 
-    def compute_sobolev_matrix(self, Laplacian, epsilon, beta):
-        sobolev = Laplacian + epsilon * np.eye(Laplacian.shape[0])
-        sobolev = 0.5 * (sobolev + sobolev.T)
-        sobolev = scipy.fractional_matrix_power(sobolev, beta)
-        return torch.tensor(sobolev, dtype=torch.float32)
+    # def compute_sobolev_matrix(self, Laplacian, epsilon, beta):
+    #     sobolev = Laplacian + epsilon * np.eye(Laplacian.shape[0])
+    #     sobolev = 0.5 * (sobolev + sobolev.T)
+    #     sobolev = scipy.fractional_matrix_power(sobolev, beta)
+    #     return torch.tensor(sobolev, dtype=torch.float32)
 
     def __call__(self, x, y, mask):
         x_diff = x @ self.Dh
@@ -84,14 +49,14 @@ class SobolevLoss(BaseLoss):
 class WeightedSobolevLoss(BaseLoss):
     def __init__(self, coefficient, Laplacian, epsilon, beta, M):
         super().__init__(coefficient)
-        self.Sobolev = self.compute_sobolev_matrix(Laplacian, epsilon, beta)
+        self.Sobolev = compute_sobolev_matrix(Laplacian, epsilon, beta)
         self.Dh = create_Dh_torch(M)
 
-    def compute_sobolev_matrix(self, Laplacian, epsilon, beta):
-        sobolev = Laplacian + epsilon * np.eye(Laplacian.shape[0])
-        sobolev = 0.5 * (sobolev + sobolev.T)
-        sobolev = scipy.fractional_matrix_power(sobolev, beta)
-        return torch.tensor(sobolev, dtype=torch.float32)
+    # def compute_sobolev_matrix(self, Laplacian, epsilon, beta):
+    #     sobolev = Laplacian + epsilon * np.eye(Laplacian.shape[0])
+    #     sobolev = 0.5 * (sobolev + sobolev.T)
+    #     sobolev = scipy.fractional_matrix_power(sobolev, beta)
+    #     return torch.tensor(sobolev, dtype=torch.float32)
 
     def __call__(self, x, y, mask, lambdas):
         x_diff = x @ self.Dh
@@ -180,15 +145,20 @@ def get_loss(method, coefficient_mse, coefficient_lap, coefficient_temp, coeffic
     
     
 class primal_dual_loss:
-    def __init__(self, lambdas, x, y, mask, Dh, epsilon, beta):
-        self.coefficient = coefficient
-    
-    def primal(self):
+    def __init__(self, x, y, mask, Dh, Laplacian, epsilon, beta, alpha,lambdas, mu):
+        self.epsilon = epsilon
+        self.mu = 1.
+        self.lambdas = lambdas
+        self.Dh = Dh
+        self.Sobolev = self.compute_sobolev_matrix(x.shape[1], epsilon, beta)
+        
+    def primal(self, x, y, mask):
+        
+        loss_mse = self.epsilon * F.mse_loss(torch.mul(x, mask), y)
+        x_diff = x @ self.Dh
+        loss_sob = torch.trace(x_diff.T @ self.Sobolev @ x_diff)
         return self.lambdas
     
-    def __call__(self, x, y, mask):
-        raise NotImplementedError("Each loss must implement the __call__ method.")
-
     def dual(lambdas, x, y, mask, Dh, epsilon, beta):
         """
         Update the lambdas using the primal-dual method.
