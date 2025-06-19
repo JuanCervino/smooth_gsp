0ccc5e12e58adb3a043ff0c169ebd131a42713dd
Juan Cervino <juancervino@dhcp-10-29-216-39.dyn.MIT.EDU>
2025-06-19 18:09:26 -0400

    Added the waether dataset

--- graph_tools/load_dataset.py
+++ graph_tools/load_dataset.py
@@ -1,8 +1,9 @@
 import numpy as np
 from scipy.io import loadmat, savemat
-from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix, csr_matrix
 from sklearn.neighbors import NearestNeighbors
-
+import netCDF4
+import matplotlib.pyplot as plt
 
 
 def load_dataset(graph, knn_param=10):
@@ -45,6 +46,111 @@ def load_dataset(graph, knn_param=10):
         
         D = data['Data']
         return G, D
+    elif graph == 'weather':
+        # Load the file
+        folder = './datasets/run_trunc_6' 
+
+        fp='output.nc'
+        fp = folder + '/' + fp
+        nc = netCDF4.Dataset(fp)
+        data = nc.variables['vor'][:,0,:,:]  # Time, Lat, Lon
+        lat = nc.variables['lat'][:]
+        lon = nc.variables['lon'][:]
+        
+        # Debug: Print longitude range
+        print(lat.shape, lon.shape, data.shape)
+        G = create_grid_graph(lat, lon)
+        
+        # Simple flatten: reshape to (time, N) where N = lat * lon
+        data_flat = data.reshape(data.shape[0], -1)
+        # Swap time and graph dimensions
+        data_flat = data_flat.T
+  
+        # Check for dimension mismatch
+        if data_flat.shape[1] != G['N']:
+            print(f"WARNING: Data has {data_flat.shape[1]} nodes but graph has {G['N']} nodes!")
+            print("This will cause matrix multiplication errors.")
+        
+        return G, data_flat
     else:
         raise ValueError("Unsupported graph type")
         pass
+
+def create_grid_graph(lats, lons, periodic_lon=True, periodic_lat=False):
+    """
+    Create a graph from a grid of latitude and longitude points.
+    Returns a sparse adjacency matrix and coordinates.
+    
+    Args:
+        lats: Array of latitude values
+        lons: Array of longitude values
+        periodic_lon: Whether to connect the last longitude to first (across date line)
+        periodic_lat: Whether to connect the last latitude to first (across poles)
+    
+    Returns:
+        G: Dictionary containing:
+            - W: Sparse adjacency matrix (CSR format)
+            - coords: Array of shape (N, 2) with lat-lon coordinates
+            - N: Number of nodes
+            - type: String identifier
+    """
+    n_lats = len(lats)
+    n_lons = len(lons)
+    N = n_lats * n_lons
+    
+    # Create sparse adjacency matrix
+    W = lil_matrix((N, N))
+    
+    # Create coordinates array
+    coords = np.zeros((N, 2))
+    
+    # Helper function to get node index
+    def get_node_idx(i, j):
+        return i * n_lons + j
+    
+    # Fill coordinates array
+    for i in range(n_lats):
+        for j in range(n_lons):
+            node_idx = get_node_idx(i, j)
+            coords[node_idx] = [lats[i], lons[j]]
+    
+    # Connect points along same latitude (longitude lines)
+    for i in range(n_lats):
+        for j in range(n_lons - 1):
+            node1 = get_node_idx(i, j)
+            node2 = get_node_idx(i, j + 1)
+            W[node1, node2] = 1
+            W[node2, node1] = 1
+        
+        # Connect last longitude to first if periodic
+        if periodic_lon and n_lons > 1:
+            node1 = get_node_idx(i, n_lons - 1)
+            node2 = get_node_idx(i, 0)
+            W[node1, node2] = 1
+            W[node2, node1] = 1
+    
+    # Connect points along same longitude (latitude lines)
+    for j in range(n_lons):
+        for i in range(n_lats - 1):
+            node1 = get_node_idx(i, j)
+            node2 = get_node_idx(i + 1, j)
+            W[node1, node2] = 1
+            W[node2, node1] = 1
+        
+        # Connect last latitude to first if periodic
+        if periodic_lat and n_lats > 1:
+            node1 = get_node_idx(n_lats - 1, j)
+            node2 = get_node_idx(0, j)
+            W[node1, node2] = 1
+            W[node2, node1] = 1
+    
+    # Construct graph dictionary
+    G = {
+        'N': N,
+        'W': W.tocsr(),  # Convert to CSR format for efficient operations
+        'coords': coords,
+        'type': 'grid',
+        'shape': (n_lats, n_lons)  # Store grid shape for reshaping data
+    }
+    
+    return G
